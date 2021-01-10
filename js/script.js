f324819e36fd8b1573383766a3b3f80f3371bf4c
Alessandro Liggieri <smmb@live.it>
2021-01-10 12:16:28 +0100

    Esercizio funzionante che necessita pesante refactoring

--- js/script.js
+++ js/script.js
@@ -3,9 +3,15 @@ var numeriGenerati = [];
 var min = 1;
 var max = 100;
 
-for (var i = 0; i < 16; i++) {
-  numeriGenerati[i] = Math.floor(Math.random() * (max + 1 - min) + min);
+while (numeriGenerati.length < 16) {
+  var i = Math.floor(Math.random() * (max + 1 - min) + min);
+  if(numeriGenerati.indexOf(i) === -1) {
+    numeriGenerati.push(i)
+  }
 }
+numeriGenerati.sort(function(a, b) {
+  return a - b;
+});
 console.log(numeriGenerati)
 // Adesso dobbiamo chiedere all'utente di inserire un numero alla volta,
 // finchè non finiranno i numeri a disposizione, oppure finchè non ne sceglie uno dei 16 generati
@@ -14,12 +20,19 @@ var listaNumeriScelti = [];
 
 while (numeriGenerati.indexOf(numeroScelto) === -1) {
   numeroScelto = parseInt(prompt('Scegli un numero'));
-  listaNumeriScelti.push(numeroScelto);
-  if(numeriGenerati.indexOf(numeroScelto) !== -1)
+  if (listaNumeriScelti.indexOf(numeroScelto) === -1 && numeroScelto > 0 && numeroScelto < 100)  {
+    listaNumeriScelti.push(numeroScelto);
+ } else {
+   alert('devi scegliere numeri diversi inclusi tra 1 e 100')
+ }
+  if(numeriGenerati.indexOf(numeroScelto) !== -1) {
     console.log('sei esploso!')
-  else if (listaNumeriScelti.length === 5)
+    break;
+  } if (listaNumeriScelti.length === 5) {
     alert('Hai vinto!')
+    break;
+  }
 }
-var punteggio = listaNumeriScelti.length
+var punteggio = listaNumeriScelti.length -1;
 console.log(listaNumeriScelti)
 console.log('Il tuo punteggio è di ' + punteggio)
